1cbec9b4abeeabc48f8286adf7d9098ee4c54ada
Maya <maya.malaviya@gmail.com>
2019-03-06 11:21:53 -0500

    added migrations and seeds for trip related data

--- services/trips/src/db/queries.js
+++ services/trips/src/db/queries.js
@@ -8,7 +8,27 @@ function addTrip(obj) {
   return knex('trips').insert(obj);
 }
 
+function addLocation(obj) {
+  return knex('locations').insert(obj);
+}
+
+function addDestination(obj) {
+  return knex('destinations').insert(obj);
+}
+
+function addBike(obj) {
+  return knex('bike').insert(obj);
+}
+
+function addCampingGear(obj) {
+  return knex('camping_gear').insert(obj);
+}
+
 module.exports = {
   getSavedTrips,
   addTrip,
+  addLocation,
+  addDestination,
+  addBike,
+  addCampingGear,
 };
